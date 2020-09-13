c5997be6ab2c9699496126d5aa6a16158754b82c
cz495969281 <495969281@qq.com>
2020-09-13 22:21:03 +0800

    vuelidate表单验证

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,111 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item" :class="{'form-group-error':$v.name.$error}">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-inline">
+            <input type="text" name="title"  v-model.trim="$v.name.$model"  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+          </div>
+          <div class="error" v-if="!$v.name.required">用户名不得为空</div>
+          <div class="error" v-if="!$v.name.email">用户名输入格式错误</div>
+        </div>
+
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input type="password" name="title" required v-model="password" lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+          </div>
+        </div>
+
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input type="text" name="title" required  v-model="code" lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+          </div>
+          <div class="layui-form-mid svg" v-html="svg" @click="getCaptcha"></div>
+        </div>
+        <button type="button" class="layui-btn" @click="checkForm">点击登录</button>
+        <a class="imooc-link" href="http://www.layui.com">找回密码</a>
+
+      </form>
     </div>
-    <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+import { required, email } from 'vuelidate/lib/validators'
+export default {
+  name: 'app',
+  data () {
+    return {
+      svg: '',
+      name: '',
+      password: '',
+      code: '',
+      errorMsg: []
+    }
+  },
+  validations: {
+    name: {
+      required,
+      email
+    },
+    password: {
+      required
+    },
+    code: {
+      required
+    }
+  },
+  mounted () {
+    this.getCaptcha()
+  },
+  methods: {
+    getCaptcha () {
+      axios.get('http://localhost:3000/getCaptcha').then((res) => {
+        if (res.status === 200) {
+          const obj = res.data
+          if (obj.code === 200) {
+            this.svg = obj.data
+          }
+        }
+      })
+    },
+    checkForm () {
+      this.errorMsg = []
+      if (!this.name) {
+        this.errorMsg.push('登录名为空')
+      }
+      if (!this.password) {
+        this.errorMsg.push('密码不得为空')
+      }
+      if (!this.code) {
+        this.errorMsg.push('验证码为空')
+      }
     }
   }
 }
+</script>
+<style lang="scss" scoped>
+  /*scoped 在当前页面样式生效 */
+  #app{
+    background: #f2f2f2;
+  }
+  .layui-container{
+    background: #fff;
+  }
+  input{
+    width: 190px;
+  }
+  .imooc-link{
+    margin-left: 15px;
+    &:hover{
+      color: #009688;
+    }
+  }
+  .svg{
+    /*position: relative;*/
+    top: -15px;
+  }
 </style>
