25df257a1308b7014120cadeaaeaf97b201321f0
cz495969281 <495969281@qq.com>
2020-09-15 22:16:59 +0800

    vee-validate设置中文提示的第二种方式

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-import VeeValidate, { Validator } from 'vee-validate'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate, { Validator } from 'vee-validate'
+
 // 第一种方式
 // import zh from 'vee-validate/dist/locale/zh_CN'
+// 第二种
+import './local/index'
 
 Vue.use(VeeValidate)
-Validator.localize('zh-CN', zh)
+// Validator.localize('zh-CN', zh)
+
+const validator = new Validator()
+validator.localize('zh-CN')
+
 Vue.config.productionTip = false
 
 new Vue({
